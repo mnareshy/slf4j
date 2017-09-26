08224b7ade1a7228711c4e2c0193744a309e3211
Naresh M <mnaresh.y@gmai.com>
2017-09-26 22:13:21 +0530

    Intial commit - sairam

---
+++ slf4j/src/main/java/com/slf4j/samples/App.java
@@ -0,0 +1,13 @@
+package com.slf4j.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
